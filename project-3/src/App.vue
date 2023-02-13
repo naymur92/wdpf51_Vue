e05ea4fb29e63561034a1cdb33b6641666cfd3da
naymur92 <naymurrahman@yahoo.com>
2023-02-13 19:03:34 +0600

    create project-3 using vue@cli and practice from koderhq

---
+++ project-3/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-pre>
+    {{ todo }}
+  </div>
+  <div>{{ todo }}</div>
+
+  <p>{{ greetingArrow(name) }}</p>
+  <p>{{ greeting() }}</p>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      todo: [
+        {
+          name: "Naymur",
+          age: 30,
+        },
+      ],
+      name: "Naymur Rahman",
+    };
+  },
+  mounted() {
+    // axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+    //   this.todo = res.data;
+    // });
+  },
+  methods: {
+    greetingArrow: (name) => {
+      return "Hello, " + name;
+    },
+    greeting() {
+      return "Hello, " + this.name;
+    },
+  },
+};
+</script>
--- project2/src/pages/FindProduct.vue
+++ project2/src/pages/FindProduct.vue
@@ -6,19 +6,33 @@
         <a href="/products" class="btn btn-outline-warning">Back</a>
       </div>
 
-      <form @submit.prevent="findProduct(id)">
-        <input type="number" v-model.trim="id" placeholder="Enter Product Id" />
+      <form @submit.prevent="findProduct()">
+        <input
+          type="text"
+          v-model.trim="term"
+          placeholder="Enter Product Name"
+        />
+        &nbsp;
         <input
           type="submit"
           value="Search Product"
           class="btn btn-success"
-          :disabled="id < 1"
+          :disabled="term.length < 1"
         />
       </form>
 
-      <div class="card mt-5" v-if="find">
-        <div class="card-header bg-secondary">{{ product.product_name }}</div>
-        <div class="card-body">{{ product.product_details }}</div>
+      <div class="row" v-if="find">
+        <div class="my-2">
+          Total found: <strong>{{ products.length }}</strong>
+        </div>
+        <div
+          v-for="product in products"
+          :key="product.id"
+          class="col-4 card p-0 mt-5"
+        >
+          <div class="card-header bg-secondary">{{ product.product_name }}</div>
+          <div class="card-body">{{ product.product_details }}</div>
+        </div>
       </div>
     </div>
   </div>
@@ -29,18 +43,20 @@ import axios from "axios";
 export default {
   data() {
     return {
-      id: null,
+      term: "",
       find: false,
-      product: {},
+      products: [],
     };
   },
   methods: {
-    findProduct(id) {
-      axios.get("http://127.0.0.1:8000/api/products/" + id).then((res) => {
-        this.product = res.data;
-        this.find = true;
-        this.id = null;
-      });
+    findProduct() {
+      axios
+        .post("http://127.0.0.1:8000/api/search/", { item: this.term })
+        .then((res) => {
+          this.products = res.data;
+          this.find = true;
+          this.term = "";
+        });
     },
   },
 };
